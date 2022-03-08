8552368318eee90287f202dc95dbb98d196684ea
ealunah00 <96490690+ealunah00@users.noreply.github.com>
2022-03-08 16:14:37 -0600

    PRO C14

---
+++ sketch.js
@@ -0,0 +1,165 @@
+//constantes
+var PLAY = 1;
+var END = 0;
+
+//variable para el estado de juego
+var gameState = PLAY;
+
+//variables para trex y suelo
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+//variables para nubes y obstáculos
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+//variable para la puntuación
+var score;
+
+
+function preload() {
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided" , trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground", groundImage);
+  ground.x = ground.width / 2;
+  ground.velocityX = -4;  //mover esta línea al estado PLAY
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //crear grupos de obstáculos y nubes
+  obstaclesGroup = createGroup();
+  cloudsGroup = createGroup();
+  
+  score = 0;
+}
+
+function draw() {
+  background(180);
+  
+  //mostrar la puntuación
+  text("Puntuación: " + score, 500, 50);
+  score += Math.round(frameCount / 60); //mover esta línea al estado PLAY
+  
+  //configurar condición para estado PLAY
+
+    //traer línea que mueve el suelo
+    
+
+    //traer línea que suma la puntuación
+    
+    
+    if (ground.x < 0) {
+      ground.x = ground.width / 2;
+    }
+    
+    //hacer que el Trex salte al presionar la barra espaciadora
+    if(keyDown("space") && trex.y >= 150) {
+        trex.velocityY = -13;
+    }
+    
+    //agregar gravedad
+    trex.velocityY = trex.velocityY + 0.8;
+  
+    //aparecer nubes y obstáculos
+    spawnClouds();
+    spawnObstacles();
+    
+    if(obstaclesGroup.isTouching(trex)) {
+      gameState = END;
+    }
+  //configurar condición para estado END
+    
+    //detener la velocidad del suelo
+    ground.velocityX = 0;
+
+    //detener la velocidad de cada elemento de los obstáculos y de las nubes
+
+    
+
+  //evitar que el Trex caiga
+  trex.collide(invisibleGround);
+
+  drawSprites();
+}
+
+function spawnObstacles() {
+ if (frameCount % 60 === 0) {
+    var obstacle = createSprite(400, 165, 10, 40);
+    obstacle.velocityX = -6;
+    
+    //generar obstáculos al azar
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1:
+        obstacle.addImage(obstacle1);
+        break;
+      case 2:
+        obstacle.addImage(obstacle2);
+        break;
+      case 3:
+        obstacle.addImage(obstacle3);
+        break;
+      case 4:
+        obstacle.addImage(obstacle4);
+        break;
+      case 5:
+        obstacle.addImage(obstacle5);
+        break;
+      case 6:
+        obstacle.addImage(obstacle6);
+        break;
+      default:
+        break;
+    }
+   
+    //asignar escala y ciclo de vida al obstáculo          
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+   
+    //agregar cada obstáculo al grupo
+    
+  }
+}
+
+function spawnClouds() {
+  //escribir aquí el código para aparecer las nubes
+   if (frameCount % 60 === 0) {
+    cloud = createSprite(600, 100, 40, 10);
+    cloud.y = Math.round(random(10, 60));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+    //asignar ciclo de vida a la variable
+    cloud.lifetime = 134;
+    
+    //ajustar la profundidad
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+    
+    //agregar nube al grupo
+    
+  }
+}
